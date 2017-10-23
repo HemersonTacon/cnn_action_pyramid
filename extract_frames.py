be9d2d85e7dc0256dbbe367f09a42d6eb702e435
andrebto <andre.brito@ice.ufjf.br>
2017-10-23 14:37:01 -0200

    Add file convert video in frames

---
+++ extract_frames.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+import argparse
+import os
+
+encode = "utf-8"
+
+def get_Args():
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("video", action='store', type=str, help="Video path", default='list')
+        return parser.parse_args()
+
+def read_video(name):
+
+	frames = []
+	vid = cv2.VideoCapture(name)
+
+	if vid:
+		frame_count = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+		kernel = (5,5)
+
+		for i in range(frame_count):
+			ret,frame = vid.read() #ret=false if something goes wrong
+			if ret is False:
+				break;
+			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Convert frame to grayscale
+			frame = cv2.GaussianBlur(frame,kernel,0) #Gaussian blur filter to reduce noise
+			frames.append(frame)
+	return frames
+
+def write_video_frames(name, frames):
+
+	#Make a directory in the video folder with the frames
+	dir_ = 'video/' + name
+	os.makedirs(dir_, exist_ok=False)
+
+	#save frames
+	for i in range(len(frames)):
+		name_frame = dir_ + '/' + str(i) + '.png'
+		cv2.imwrite(name_frame, frames[i])
+
+def main(args):
+	
+	frames = read_video(args.video)
+	write_video_frames(args.video,frames)
+
+if __name__ == '__main__':
+	# parse arguments
+	args = get_Args()
+	main(args)
+	
