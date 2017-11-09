3ab11543b3f92141558a955d10d12bffb23f4a61
hugolimachaves <hugo.lima.chaves@gmail.com>
2017-11-09 15:58:06 -0200

    Add files via upload

--- pca.py
+++ pca.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import numpy as np
 
-global numeroDeFeaturesPrePCA
+global numero_de_features_antes_do_PCA
 global numero_de_features_apos_PCA
-numero_de_features_apos_PCA = 180
+
+
 
 
 encode = "utf-8"
@@ -17,8 +18,16 @@ def read_cnnf_file(name):
      return cnnFlows
 
 
-def auto_vetores_e_media_PCA(vetor_para_PCA,numero_de_features_apos_PCA):
-     media, autoVetores = cv.PCACompute(vetor_para_PCA, None , None, numero_de_features_apos_PCA)
+def baseground_PCA(vetor_para_PCA, numero_de_features_apos_PCA):
+     
+     print("\ncomputando os autovetores e o vetor de media... pode demorar...")
+     print(type(vetor_para_PCA))
+     print("Debug dentro de baseground...")
+     print("O numero de linhas é: " + str(len(vetor_para_PCA)))
+     print("O numero de colunas é: " + str(len(vetor_para_PCA[0])))
+
+     print("O numero de features apos PCA e de: " + str(numero_de_features_apos_PCA))
+     media, autoVetores = cv.PCACompute(vetor_para_PCA, None , None, int(numero_de_features_apos_PCA))
      return autoVetores, media
 
 def projecao_PCA(vetor_para_PCA, media, auto_vetores):
@@ -30,54 +39,69 @@ def back_projecao_PCA(projecao, media, auto_vetores):
      return back_projecao
 
 def erro_medio_de_projecao(vetor_para_PCA,back_projecao):
-     erro = vetor_para_PCA - back_projecao
+     erro = (vetor_para_PCA - back_projecao)
      erro_quadratico_medio = np.mean(np.power(erro,2))
      return erro_quadratico_medio
     	
-def write_cnn_flow(name, cnn_flow_PCA):
+def write_pca_reduction(name, cnn_flow_PCA):
 
      out_file = name + ".pca"
-	
      # With automatically closes output
      with open(out_file, "w", encoding=encode) as output:
 	  # Joining cnn flows elements with space and then joining cnn flows with \n and finally joining snippets with \n\n
-               output.write("\n\n".join(["\n".join([" ".join(list(map(str, j))) for j in i]) for i in cnn_flow_PCA]))
+               output.write("\n".join([" ".join(list(map(str,i))) for i in baseground_PCA]))
 
 
-#nao
-def write_pca_reduction(name, outdir, cnnFlowPCA):
+def write_pca_baseground(name, baseground_PCA):
 
-     out_file = outdir + name + ".pca"
+     out_file = name + ".pcab"	
+     # With automatically closes output
      with open(out_file, "w", encoding=encode) as output:
-     # Joining frames binary coded with \n
-          output.write("\n".join(cnnFlowPCA))
+	  # Joining cnn flows elements with space and then joining cnn flows with \n and finally joining snippets with \n\n
+               output.write("\n".join([" ".join(list(map(str,i))) for i in baseground_PCA]))
 
+     
 def cnn_flow_para_PCA(cnn_flow):
      global numero_de_features_antes_do_PCA
+     global numero_de_features_apos_PCA
      cnnFlowsSplit = []
      cnnFlowsSplitFloat = [[]]
      contador = -1
+     
      for i in range(len(cnn_flow)):
+
           cnnFlowsSplit = cnn_flow[i].split( )
+
           if cnnFlowsSplit == []:
+
                continue
           contador = contador + 1
           cnnFlowsSplitFloat.append([]) # gerando um novo no para a lista
+
           for j in range (len(cnnFlowsSplit)): # casting
+
                cnnFlowsSplitFloat[contador].append( float(cnnFlowsSplit[j]))  
+
      del cnnFlowsSplitFloat[len(cnnFlowsSplitFloat) - 1] # REMOVENDO O NO EXTRA GERADO
      vetor_para_PCA = np.array(cnnFlowsSplitFloat)
      numero_de_features_antes_do_PCA= len(vetor_para_PCA[0])
+     numero_de_features_apos_PCA = numero_de_features_antes_do_PCA/2 #voltar depois para 180
      return vetor_para_PCA
 
+
 def main():
-     
+     global numero_de_features_antes_do_PCA
+     global numero_de_features_apos_PCA
      cnn_flow = read_cnnf_file("cnnflows")
      vetor_para_PCA = cnn_flow_para_PCA(cnn_flow)
-     auto_vetores, media = auto_vetores_e_media_PCA(vetor_para_PCA,numero_de_features_apos_PCA)
+     print("Numero de features antes do PCA: " + str(numero_de_features_antes_do_PCA))
+     print("Numero de features apos o PCA: " + str(numero_de_features_apos_PCA))
+     auto_vetores, media = baseground_PCA(vetor_para_PCA,numero_de_features_apos_PCA)
+     write_pca_baseground("autovetores", auto_vetores)
+     write_pca_baseground("vetor_media", media)
      projecao = projecao_PCA(vetor_para_PCA, media, auto_vetores)
      back_projecao = back_projecao_PCA(projecao, media, auto_vetores)
      erro_quadratico_medio = erro_medio_de_projecao(vetor_para_PCA,back_projecao)
-     print(erro_quadratico_medio)     
+          
 if __name__ == '__main__':
      main()
